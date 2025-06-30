01a7980f9f1e3d199157875ddb6fd066b6c97ade
PietroAngiolla <pietroangiolla@gmail.com>
2025-06-30 23:30:51 +0200

    V.0.2 concerti inseriti+barra di ricerca funzionante

--- Homepage/script.js
+++ Homepage/script.js
@@ -2,107 +2,128 @@ const concerts = [
     {
         Artista: "Massimo Di Cataldo",
         Data: "21 Luglio",
-        Luogo: "Peschici"
+        Luogo: "Peschici",
+        Prezzo: "Gratis"
     },
     {
         Artista: "Rocco Hunt",
         Data: "26 Luglio",
         Luogo: "Peschci",
+        Prezzo: "40,25€"
     },
     {
         Artista: "Danilo Sacco",
         Data: "28 Luglio",
-        Luogo: "Roseto Valforte"
+        Luogo: "Roseto Valforte",
+        Prezzo: "Gratis"
     },
     {
         Artista: "Stewart Copeland",
         Data: "29 Luglio",
-        Luogo: "Foggia"
+        Luogo: "Foggia",
+        Prezzo: "Da 28,75€"
     },
     {
         Artista: "The Kolors",
         Data: "1 Agosto",
-        Luogo: "Vieste"
+        Luogo: "Vieste",
+        Prezzo: "Gratis"
     },
     {
         Artista: "Patty Bravo",
         Data: "2 Agosto",
-        Luogo: "Vieste"
+        Luogo: "Vieste",
+        Prezzo: "Gratis"
     },
     {
         Artista: "Fabri Fibra",
         Data: "3 Agosto",
-        Luogo: "Vieste"
+        Luogo: "Vieste",
+        Prezzo: "Da 39€"
     },
     {
         Artista: "Dirotta Su Cuba",
         Data: "14 Agosto",
-        Luogo: "Stornarella"
+        Luogo: "Stornarella",
+        Prezzo: "Gratis"
     },
     {
         Artista: "Nek",
         Data: "15 Agosto",
-        Luogo: "Lucera"
+        Luogo: "Lucera",
+        Prezzo: "Gratis"
     },
     {
         Artista: "Max Gazzè",
         Data: "16 Agosto",
-        Luogo: "Lucera"
+        Luogo: "Lucera",
+        Prezzo: "Gratis"
     },
     {
         Artista: "The Kolors",
         Data: "17 Agosto",
-        Luogo: "Lucera"
+        Luogo: "Lucera",
+        Prezzo: "Gratis"
     },
     {
         Artista: "Coma Cose",
         Data: "18 Agosto",
-        Luogo: "Ischitella"
+        Luogo: "Ischitella",
+        Prezzo: "Gratis"
     },
     {
         Artista: "Gianna Nannini",
         Data: "18 Agosto",
-        Luogo: "Apricena"
+        Luogo: "Apricena",
+        Prezzo: "Da 47€"
     },
     {
         Artista: "Nino D'Angelo",
         Data: "19 Agosto",
-        Luogo: "Apricena"
+        Luogo: "Apricena",
+        Prezzo: "Da 50€"
     },
     {
         Artista: "Nina Zilli",
         Data: "19 Agosto",
-        Luogo: "Carpino"
+        Luogo: "Carpino",
+        Prezzo: "Gratis"
     },
     {
         Artista: "Tony Effe",
         Data: "21 Agosto",
-        Luogo: "Apricena"
+        Luogo: "Apricena",
+        Prezzo: "40,25€"
     },
     {
         Artista: "Tiromancino",
         Data: "21 Agosto",
-        Luogo: "Rodi Garganico"
+        Luogo: "Rodi Garganico",
+        Prezzo: "Gratis"
     },
     {
         Artista: "Fiorella Mannoia",
         Data: "22 Agosto",
-        Luogo: "Apricena"
+        Luogo: "Apricena",
+        Prezzo: "Da 28,75€"
     },
     {
         Artista: "Dolcenera",
         Data: "22 Agosto",
-        Luogo: "Accadia"
+        Luogo: "Accadia",
+        Prezzo: "Gratis"
     },
     {
         Artista: "Alex Britti",
         Data: "26 Agosto",
-        Luogo: "Vico Del Gargano"
+        Luogo: "Vico Del Gargano",
+        Prezzo: "Gratis"
     },
     {
         Artista: "Raphael Gualazzi",
         Data: "6 Settembre",
-        Luogo: "Peschici"
+        Luogo: "Peschici",
+        Prezzo: "Gratis"
     }
 ]
 
@@ -111,13 +132,26 @@ function showConcerts(list) {
     list.forEach(concerto => {
         const card = document.createElement("div")
         card.classList.add("card-body", "col-6", "col-md-4", "col-lg-3")
-        card.innerHTML = `
-            <h5 class="card-title">${concerto.Artista}</h5>
-            <h6 class="card-subtitle mb-2 text-body-secondary">${concerto.Data}</h6>
-            <p class="card-text">Luogo: ${concerto.Luogo}</p>
-            <a href="#" class="card-link">info concerto</a>
-            <a href="#" class="card-link">Prezzo: ${concerto.Prezzo}</a>
-        `
+        if (concerto.Prezzo!=="Gratis"){
+             card.innerHTML = `
+                <h5 class="card-title">${concerto.Artista}</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">${concerto.Data}</h6>
+                <p class="card-text">Luogo: ${concerto.Luogo}</p>
+                <a href="#" class="card-link">info concerto</a>
+                <a href="#" class="card-link">Prezzo: ${concerto.Prezzo}</a>
+            `
+        }else{
+             card.innerHTML = `
+                <h5 class="card-title">${concerto.Artista}</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">${concerto.Data}</h6>
+                <p class="card-text">Luogo: ${concerto.Luogo}</p>
+                <div class="flex-link">
+                    <a href="#" class="card-link">info concerto</a>
+                    <p>Prezzo: ${concerto.Prezzo}</p>
+                </div>
+            `
+        }
+       
         row.appendChild(card)
     });
 }

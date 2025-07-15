19c0e57df6ce9aedc6596b537318943caef8f34c
PietroAngiolla <pietroangiolla@gmail.com>
2025-07-15 17:25:07 +0200

    css route

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'Homepage')));
 app.use(express.static(path.join(__dirname, 'Agosto')));
 app.use(express.static(path.join(__dirname, 'Luglio')));
 app.use(express.static(path.join(__dirname, 'Settembre')));
-
+app.use('/css', express.static(path.join(__dirname, 'Homepage')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'Homepage', 'index.html'));
